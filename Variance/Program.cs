b45ed2a800b04d9c264d64df7b646ca24fb00b50
hyeonwoo <escape0619@gmail.com>
2021-03-29 20:03:47 +0900

    first commit

---
+++ Variance/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Variance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 데이터가 없을 경우 혹은 1개 일 경우 에는 분산을 계산할 수 없다.
+            if (args.Length == 0) {
+                Console.WriteLine("데이터가 입력되지 않았습니다.");
+                return;
+            } else if (args.Length == 1){
+                Console.WriteLine("데이터를 두개이상 입력하시오.");
+                return;
+            }
+
+            double[] s = new double[args.Length];
+            for (int i = 0; i < s.Length; i++) {
+                s[i] = double.Parse(args[i]);
+            }
+            double sum = 0.0;
+
+            for (int i = 0; i < s.Length; i++){
+                sum += s[i];
+            }
+
+            double mean = sum / s.Length;
+            double sumOfSquares = 0.0;
+            for (int i = 0; i < s.Length; i++){
+                sumOfSquares += ( s[i] - mean ) * ( s[i] - mean );
+            }
+            double variance = sumOfSquares / (s.Length - 1);
+            Console.WriteLine($"분산 : {variance}");
+        }
+    }
+}
