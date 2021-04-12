d9bbe96570f04e6b2409a8fa8613c452d60b84c7
hyeonwoo <escape0619@gmail.com>
2021-04-12 21:32:09 +0900

    Implements SinglePlayer mode with TDD

---
+++ NumberGuessing/console-app-model/src/test/java/numberguessing/PositiveIntegerGeneratorStub.java
@@ -0,0 +1,20 @@
+package numberguessing;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class PositiveIntegerGeneratorStub implements PositiveIntegerGenerator {
+    private final int[] numbers;
+    private int index;
+
+    public PositiveIntegerGeneratorStub(int... numbers) {
+        this.numbers = numbers;
+        index = 0;
+    }
+
+    @Override
+    public int generateLessThanOrEqualToHundred() {
+        int number = numbers[index];
+        index = (index + 1) % numbers.length;
+        return number;
+    }
+}
