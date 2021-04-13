6071d411752de6aa20680f31e5cb8f77f1f53cbf
escape0619@gmail.com <hwchoi@ytcreatives.net>
2021-04-13 09:01:01 +0900

    wip) multimode implement

--- NumberGuessing/console-app-model/src/main/java/numberguessing/console/AppModel.java
+++ NumberGuessing/console-app-model/src/main/java/numberguessing/console/AppModel.java
@@ -2,6 +2,8 @@ package numberguessing.console;
 
 import numberguessing.PositiveIntegerGenerator;
 
+import java.util.stream.Stream;
+
 public final class AppModel {
     private static final String NEW_LINE = System.lineSeparator();
     public static final String SELECT_MODE_MESSAGE = "1: Single player game" + NEW_LINE + "2: Multiplayer game" + NEW_LINE + "3: Exit"
@@ -13,13 +15,13 @@ public final class AppModel {
 
     private final PositiveIntegerGenerator generator;
     private boolean completed;
-    private String output;
+    private StringBuffer outputBuffer;
     private Processor processor;
 
     public AppModel(PositiveIntegerGenerator generator) {
         this.generator = generator;
+        this.outputBuffer = new StringBuffer(SELECT_MODE_MESSAGE);
         completed = false;
-        output = SELECT_MODE_MESSAGE;
         processor = this::processModeSelection;
     }
 
@@ -28,19 +30,40 @@ public final class AppModel {
     }
 
     public String flushOutput() {
+        String output = outputBuffer.toString();
+        outputBuffer.setLength(0);
         return output;
     }
 
     private Processor processModeSelection(String input) {
         if (input.equals("1")) {
-            output = "Single player game" + NEW_LINE + "I'm thinking of a number between 1 and 100."
-                    + NEW_LINE + "Enter your guess: ";
+            outputBuffer.append("Single player game" + NEW_LINE + "I'm thinking of a number between 1 and 100."
+                    + NEW_LINE + "Enter your guess: ");
             int answer = generator.generateLessThanOrEqualToHundred();
             return getSinglePlayerGameProcessor(answer,1);
+        } else if (input.equals("2")) {
+            outputBuffer.append("Multiplayer game" + NEW_LINE + "Enter player names separated with commas: ");
+            return getMultiplayerGameProcessor();
         } else {
             completed = true;
+            return null;
         }
-        return null;
+    }
+
+    private Processor getMultiplayerGameProcessor() {
+        return input -> {
+            Object[] players = Stream.of(input.split(","))
+                    .map(String::trim).toArray();
+            outputBuffer.append("I'm thinking of a number between 1 and 100.");
+            outputBuffer.append("Enter "+players[0]+"'s guess: ");
+            return input2 -> {
+                outputBuffer.append("Enter "+ players[1] +"'s guess: ");
+                return input3 -> {
+                    outputBuffer.append("Enter "+ players[2] +"'s guess: ");
+                    return null;
+                };
+            };
+        };
     }
 
     public void processInput(String input){
@@ -52,13 +75,13 @@ public final class AppModel {
             int guess = Integer.parseInt(input);
 
             if (guess < answer) {
-                output = "Your guess is too low." + NEW_LINE + "Enter your guess: ";
+                outputBuffer.append("Your guess is too low." + NEW_LINE + "Enter your guess: ");
                 return getSinglePlayerGameProcessor(answer,tries + 1);
             } else if (guess > answer) {
-                output = "Your guess is too high." + NEW_LINE + "Enter your guess: ";
+                outputBuffer.append("Your guess is too high." + NEW_LINE + "Enter your guess: ");
                 return getSinglePlayerGameProcessor(answer,tries + 1);
             } else {
-                output = "Correct! Done " + tries + (tries == 1 ? " guess." : " guesses.") + NEW_LINE + SELECT_MODE_MESSAGE;
+                outputBuffer.append("Correct! Done " + tries + (tries == 1 ? " guess." : " guesses.") + NEW_LINE + SELECT_MODE_MESSAGE);
                 return this::processModeSelection;
             }
         };
--- NumberGuessing/console-app-model/src/test/java/numberguessing/console/AppModelTest_specs.java
+++ NumberGuessing/console-app-model/src/test/java/numberguessing/console/AppModelTest_specs.java
@@ -160,4 +160,66 @@ class AppModelTest_specs {
         assertThat(actual).startsWith("Correct! ");
     }
 
+    @Test
+    void sut_correctly_prints_multiplayer_game_setup_message() {
+        var sut = new AppModel(new PositiveIntegerGeneratorStub(50));
+        sut.flushOutput();
+        sut.processInput("2");
+
+        String actual = sut.flushOutput();
+
+        assertThat(actual).isEqualTo("Multiplayer game" + NEW_LINE + "Enter player names separated with commas: ");
+    }
+
+    @Test
+    void sut_correctly_prints_multiplayer_game_start_message() {
+        var sut = new AppModel(new PositiveIntegerGeneratorStub(50));
+        sut.processInput("2");
+        sut.flushOutput();
+        sut.processInput("Foo, Bar");
+
+        String actual = sut.flushOutput();
+
+        assertThat(actual).startsWith("I'm thinking of a number between 1 and 100.");
+    }
+
+    @ParameterizedTest
+    @CsvSource({ "Foo, Bar, Baz", "Bar, Baz, Foo", "Baz, Foo, Bar" })
+    void sut_correctly_prompts_first_player_name(String player1, String player2, String player3) {
+        var sut = new AppModel(new PositiveIntegerGeneratorStub(50));
+        sut.processInput("2");
+        sut.flushOutput();
+        sut.processInput(String.join(", ", player1, player2, player3));
+
+        String actual = sut.flushOutput();
+
+        assertThat(actual).endsWith("Enter " + player1 + "'s guess: ");
+    }
+    @ParameterizedTest
+    @CsvSource({ "Foo, Bar, Baz", "Bar, Baz, Foo", "Baz, Foo, Bar" })
+    void sut_correctly_prompts_second_player_name(String player1, String player2, String player3) {
+        var sut = new AppModel(new PositiveIntegerGeneratorStub(50));
+        sut.processInput("2");
+        sut.processInput(String.join(", ", player1, player2, player3));
+        sut.flushOutput();
+        sut.processInput("10");
+
+        String actual = sut.flushOutput();
+
+        assertThat(actual).endsWith("Enter " + player2 + "'s guess: ");
+    }
+    @ParameterizedTest
+    @CsvSource({ "Foo, Bar, Baz", "Bar, Baz, Foo", "Baz, Foo, Bar" })
+    void sut_correctly_prompts_third_player_name(String player1, String player2, String player3) {
+        var sut = new AppModel(new PositiveIntegerGeneratorStub(50));
+        sut.processInput("2");
+        sut.processInput(String.join(", ", player1, player2, player3));
+        sut.processInput("90");
+        sut.flushOutput();
+        sut.processInput("90");
+
+        String actual = sut.flushOutput();
+
+        assertThat(actual).endsWith("Enter " + player3 + "'s guess: ");
+    }
 }
