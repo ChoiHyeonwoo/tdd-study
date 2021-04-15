f5b272663ec313f20cecda679694f4f1ce8acb85
hyeonwoo <escape0619@gmail.com>
2021-04-15 22:08:03 +0900

    multiplayer mode game with tdd done

--- NumberGuessing/console-app-model/src/main/java/numberguessing/console/AppModel.java
+++ NumberGuessing/console-app-model/src/main/java/numberguessing/console/AppModel.java
@@ -35,34 +35,61 @@ public final class AppModel {
         return output;
     }
 
+    private void println(String message) {
+        outputBuffer.append(message + NEW_LINE);
+    }
+
+    private void print(String message) {
+        outputBuffer.append(message);
+    }
+
+
     private Processor processModeSelection(String input) {
         if (input.equals("1")) {
-            outputBuffer.append("Single player game" + NEW_LINE + "I'm thinking of a number between 1 and 100."
-                    + NEW_LINE + "Enter your guess: ");
+            println("Single player game");
+            println("I'm thinking of a number between 1 and 100.");
+            print("Enter your guess: ");
             int answer = generator.generateLessThanOrEqualToHundred();
             return getSinglePlayerGameProcessor(answer,1);
         } else if (input.equals("2")) {
-            outputBuffer.append("Multiplayer game" + NEW_LINE + "Enter player names separated with commas: ");
-            return getMultiplayerGameProcessor();
+            println("Multiplayer game");
+            print("Enter player names separated with commas: ");
+            return startMultiplayerGame();
         } else {
             completed = true;
             return null;
         }
     }
 
-    private Processor getMultiplayerGameProcessor() {
+
+    private Processor startMultiplayerGame() {
         return input -> {
             Object[] players = Stream.of(input.split(","))
                     .map(String::trim).toArray();
-            outputBuffer.append("I'm thinking of a number between 1 and 100.");
-            outputBuffer.append("Enter "+players[0]+"'s guess: ");
-            return input2 -> {
-                outputBuffer.append("Enter "+ players[1] +"'s guess: ");
-                return input3 -> {
-                    outputBuffer.append("Enter "+ players[2] +"'s guess: ");
-                    return null;
-                };
-            };
+            int answer = generator.generateLessThanOrEqualToHundred();
+            print("I'm thinking of a number between 1 and 100.");
+            return getMultiplayerGameProcessor(players, answer, 1);
+        };
+    }
+
+    private Processor getMultiplayerGameProcessor(Object[] players, int answer, int tries) {
+        Object player = players[(tries - 1) % players.length];
+        print("Enter "+ player + "'s guess: ");
+        return input -> {
+            int guess = Integer.parseInt(input);
+
+            if (guess < answer) {
+                println(player+"'s guess is too low.");
+                return getMultiplayerGameProcessor(players, answer, tries + 1);
+            } else if (guess > answer) {
+                println(player+"'s guess is too high.");
+                return getMultiplayerGameProcessor(players, answer, tries + 1);
+            } else {
+                print("Correct! ");
+                println(player + " wins.");
+                print(SELECT_MODE_MESSAGE);
+                return this::processModeSelection;
+            }
         };
     }
 
@@ -75,13 +102,16 @@ public final class AppModel {
             int guess = Integer.parseInt(input);
 
             if (guess < answer) {
-                outputBuffer.append("Your guess is too low." + NEW_LINE + "Enter your guess: ");
+                println("Your guess is too low.");
+                print("Enter your guess: ");
                 return getSinglePlayerGameProcessor(answer,tries + 1);
             } else if (guess > answer) {
-                outputBuffer.append("Your guess is too high." + NEW_LINE + "Enter your guess: ");
+                println("Your guess is too high.");
+                print("Enter your guess: ");
                 return getSinglePlayerGameProcessor(answer,tries + 1);
             } else {
-                outputBuffer.append("Correct! Done " + tries + (tries == 1 ? " guess." : " guesses.") + NEW_LINE + SELECT_MODE_MESSAGE);
+                println("Correct! Done " + tries + (tries == 1 ? " guess." : " guesses."));
+                print(SELECT_MODE_MESSAGE);
                 return this::processModeSelection;
             }
         };
